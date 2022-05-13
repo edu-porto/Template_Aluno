2c2bd9666bdcdf1af947ef26bda1106c30a86b01
edu-porto <eduardo.porto@sou.inteli.edu.br>
2022-05-13 19:38:48 -0300

    A função guarda o dado mas nao printa

--- 03_AUT_EST_ENTREGA/Semana 4/script.js
+++ 03_AUT_EST_ENTREGA/Semana 4/script.js
@@ -1,12 +1,25 @@
-﻿var experiencias = [];
+﻿var ListaExperiencias = [];
+var anotarExp = {
+  frase: "",
+  testando: 0
+};
 
 function anotarExperiencia(){
-  var tipoExp = document.getElementById("ExperienciaUser").value;
-  console.log(tipoExp)
-
-
+  var nome = document.getElementById("ExperienciaUser").value;
+  console.log(nome);
+  var experiencia = Object.create(frase);
+  experiencia.frase = nome;
+  ListaExperiencias.push(experiencia);
 };
 
 function mostrarExperiencia(){
-  
-}
+  var lista = "<table>";
+  for (var i=0 ; i<ListaExperiencias.length ; i++){
+    lista += "<tr>";
+    lista += "<td>" + ListaExperiencias[i].frase + "</td>";
+    lista += "</tr>";
+  };
+  lista += "</table>";
+  document.getElementById("listaexperiencias2").innerHTML = lista;
+  console.log(ListaExperiencias);
+};

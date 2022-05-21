755a686f95d52ab1e6a477c5194e5ea3b5be86a2
edu-porto <eduardo.porto@sou.inteli.edu.br>
2022-05-20 21:35:39 -0300

    Exercicio 1 concluido

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/script1.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/script1.js
@@ -1,54 +1,51 @@
-var ListaExperiencias = [];
-var anotarExp = {
-  frase: ""
-};
 
-var listaB = [];
-var anotarB = {
-    numeroB: ""
-};
+function subtrair(){
+  var valor1 = document.getElementById("numeroA").value;
+  var valor2 = document.getElementById("numeroB").value;
 
-function anotarNumeroB (){
-    var valor2 = document.getElementById("numeroB").value;
-    var valorB = Object.create(listaB);
-    valorB.numeroB =valor2;
-    listaB.push(valorB);
-    console.log(listaB)
-};
-function anotarExperiencia(){
-  var nome = document.getElementById("ExperienciaUser").value;
-  var experiencia = Object.create(anotarExp);
-  experiencia.frase = nome;
-  ListaExperiencias.push(experiencia);
-  console.log(nome);
-};
-//A função anotar grave os inputs do usúario em uma lista 
+  var total1 = valor1 - valor2
+
+  var resultados = "<h1>" ;
+  document.getElementById("resultados").innerHTML = resultados + total1 ;
+}
 
 function somar(){
-    var A = ListaExperiencias;
-    var B = listaB;
-    var ultimoA = A[A.length - 1];
-    var ultimoB = B[B.length - 1];
+  var valor1 = document.getElementById("numeroA").value;
+  var valor2 = document.getElementById("numeroB").value;
+  var total2 = parseInt(valor1) + parseInt(valor2)
+  var resultados = "<h1>" ;
+  document.getElementById("resultados").innerHTML = resultados + total2 ;
+}
 
-    
-    var total = ultimoA + ultimoB
+function dividir (){
+  var valor1 = document.getElementById("numeroA").value;
+  var valor2 = document.getElementById("numeroB").value;
+  var total3 = valor1 / valor2
+  var resultados = "<h1>" ;
+  document.getElementById("resultados").innerHTML = resultados + total3 ;
+}
 
-    console.log(total)
-    
-   //19:26 consegui converter os elementos da lista mais da um erro NaN 
-    
-};
-// estou com dificuldade em pegar o ultimo elemento da array, transformar de string pra numero e somar 
+function quociente(){
+  var valor1 = document.getElementById("numeroA").value;
+  var valor2 = document.getElementById("numeroB").value;
+  var total4 = Math.floor(valor1/valor2)
+ 
+  var resultados = "<h1>" ;
+  document.getElementById("resultados").innerHTML = resultados + total4 ;
+}
 
+function porcentagem(){
+  var valor1 = document.getElementById("numeroA").value;
+  var valor2 = document.getElementById("numeroB").value;
 
-function mostrarExperiencia(){
-  var lista = "<table>";
-  for (var i=0 ; i<ListaExperiencias.length ; i++){
-    lista += "<tr>";
-    lista += "<td>" + ListaExperiencias[i].frase + "</td>";
-    lista += "</tr>";
-  };
-  lista += "</table>";
-  document.getElementById("listaexperiencias2").innerHTML = lista;
-  console.log(ListaExperiencias);
-};
+  var regraTres = parseInt(valor1)*100
+  var total5 = parseInt(regraTres)/parseInt(valor2)
+  
+
+  var resultados = "<h1>" ;
+  document.getElementById("resultados").innerHTML = resultados + total5 ;
+
+}
+// todas as funções pegam o input do usúario, salvam como uma variavel.
+// logo em seguida é criada uma variavel chamada resultados que serve como um header de html
+// nela e mandado para o html o total da operação inputada pelo usuario e o header

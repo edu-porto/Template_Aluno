8e240aed35bff1c636884b1e238cc1ebb046e810
edu-porto <eduardo.porto@sou.inteli.edu.br>
2022-05-27 15:15:52 -0300

    Modulo get adicionado

--- 03_AUT_EST_ENTREGA/Semana 6/backend/app_22.js
+++ 03_AUT_EST_ENTREGA/Semana 6/backend/app_22.js
@@ -6,43 +6,31 @@ const hostname = '127.0.0.1';
 const port = 3041;
 const sqlite3 = require('sqlite3').verbose(); 
 const DBPATH = 'dbUser.db'; 
+
 const bodyParser = require('body-parser');
+
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(express.static("../frontend/"));
 
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
+//adicionando o comando get no CRUD
 
-    var db = new sqlite3.Database(DBPATH);
-	var sql = 'SELECT * FROM tbUser ORDER BY title COLLATE NOCASE';
-    db.all(sql, [],  (err, rows ) => {
-        if (err) {
-            throw err;
-        }
-        res.json(rows);
-    });
-    db.close();
+app.get('/informacoes', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	var db = new sqlite3.Database(DBPATH); 
+  var sql = 'SELECT * FROM infos ORDER BY id_info COLLATE NOCASE'
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); 
 });
-
-
-app.post('/userinsert', urlencodedParser, (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-
-    sql = "INSERT INTO tbUser (title, id, completed) VALUES ('" + req.body.title + "', 33, false)";
-    var db = new sqlite3.Database(DBPATH); 
-    db.run(sql, [],  err => {
-        if (err) {
-            throw err;
-        }
-    });
-    db.close();
-    res.end();
-});
-
+// quando o usuario entra na aba informacoes há acesso a informações adicionais sobre o curriculum
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });

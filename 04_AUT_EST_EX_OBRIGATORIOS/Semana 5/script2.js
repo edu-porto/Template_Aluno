8897fb3b753e09f810b00b63c9a488bdd9cc94e7
edu-porto <eduardo.porto@sou.inteli.edu.br>
2022-06-01 14:56:34 -0300

    exercicio 2 quase prontp

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/script2.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/script2.js
@@ -3,42 +3,23 @@
 function testar_logica(){
   var inputNoob = document.getElementById("numeroA").value;
 
-  var listaDeNotas= [1,2,5,10,20,50,100];
-  
-  if (listaDeNotas[0] > inputNoob){
-    alert("Hello World")
-  };
+  var resutltado = (inputNoob-(inputNoob%100))/100 //aqui me da quantas notas de 100  preciso
+  var sobraNotas = (inputNoob%100) //numero que eh pra sobrar
 
-  var troco = inputNoob / listaDeNotas[3]
+  var testando = (sobraNotas-(sobraNotas%50))/50 //notas de 50 necessitam 
+  
+  var resto50 = sobraNotas%50 
+
+  var testando2 = (resto50-(resto50%20))/20
+  
+  
+  
+
+
+  
   // PRECISO ENCONTRAR UM METODO PARA COMPARAR COM OS VALORES INDIVIDUAIS DE CADA OBJETO
   // DAR UMA OLHADA AFUNDO NESSA SOLUÇAO https://pt.stackoverflow.com/questions/398871/calcular-troco-e-exibir-notas-dispon%C3%ADveis-com-javascript  
-  console.log(re)
-}
-
-function subtrair(){
-  var valor1 = document.getElementById("numeroA").value;
-  var valor2 = document.getElementById("numeroB").value;
-
-  var total1 = valor1 - valor2
-
-  var resultados = "<h1>" ;
-  document.getElementById("resultados").innerHTML = resultados + total1 ;
-}
-
-function somar(){
-  var valor1 = document.getElementById("numeroA").value;
-  var valor2 = document.getElementById("numeroB").value;
-  var total2 = parseInt(valor1) + parseInt(valor2)
-  var resultados = "<h1>" ;
-  document.getElementById("resultados").innerHTML = resultados + total2 ;
-}
-
-function dividir (){
-  var valor1 = document.getElementById("numeroA").value;
-  var valor2 = document.getElementById("numeroB").value;
-  var total3 = valor1 / valor2
-  var resultados = "<h1>" ;
-  document.getElementById("resultados").innerHTML = resultados + total3 ;
+  console.log(resutltado, testando,testando2)
 }
 
 function quociente(){

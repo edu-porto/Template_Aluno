6f5341b176d8b04c923540acf432845cebec0f9f
edu-porto <eduardo.porto@sou.inteli.edu.br>
2022-06-02 14:45:06 -0300

    só falta arrumar o texto que sera mostrado na tela

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/script2.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/script2.js
@@ -1,48 +1,35 @@
 
 
 function testar_logica(){
-  var inputNoob = document.getElementById("numeroA").value;
+  var inputUser = document.getElementById("numeroA").value;
 
-  var resutltado = (inputNoob-(inputNoob%100))/100 //aqui me da quantas notas de 100  preciso
-  var sobraNotas = (inputNoob%100) //numero que eh pra sobrar
+  var testando100 = (inputUser-(inputUser%100))/100 //aqui me da quantas notas de 100  preciso
+  inputUser = (inputUser%100) //numero que eh pra sobrar
 
-  var testando = (sobraNotas-(sobraNotas%50))/50 //notas de 50 necessitam 
+  var testando50 = (inputUser-(inputUser%50))/50 //notas de 50 necessitam 
+  inputUser = inputUser%50 
+
+  var testando20 = (inputUser-(inputUser%20))/20 //notas de 20 que precisa
+  inputUser = inputUser%20
   
-  var resto50 = sobraNotas%50 
+  var testando10 = (inputUser-(inputUser%10))/10  //notas de 10 que precisa
+  inputUser = inputUser%10
 
-  var testando2 = (resto50-(resto50%20))/20
-  
-  
-  
+  var testando5 = (inputUser-(inputUser%5))/5
+  inputUser = inputUser%5
 
+  var testando2 = (inputUser-(inputUser%2))/2
+  inputUser = inputUser%2
 
-  
-  // PRECISO ENCONTRAR UM METODO PARA COMPARAR COM OS VALORES INDIVIDUAIS DE CADA OBJETO
-  // DAR UMA OLHADA AFUNDO NESSA SOLUÇAO https://pt.stackoverflow.com/questions/398871/calcular-troco-e-exibir-notas-dispon%C3%ADveis-com-javascript  
-  console.log(resutltado, testando,testando2)
-}
+  var testando1 = (inputUser-(inputUser%1))/1
 
-function quociente(){
-  var valor1 = document.getElementById("numeroA").value;
-  var valor2 = document.getElementById("numeroB").value;
-  var total4 = Math.floor(valor1/valor2)
- 
-  var resultados = "<h1>" ;
-  document.getElementById("resultados").innerHTML = resultados + total4 ;
-}
-
-function porcentagem(){
-  var valor1 = document.getElementById("numeroA").value;
-  var valor2 = document.getElementById("numeroB").value;
-
-  var regraTres = parseInt(valor1)*100
-  var total5 = parseInt(regraTres)/parseInt(valor2)
-  
 
   var resultados = "<h1>" ;
-  document.getElementById("resultados").innerHTML = resultados + total5 ;
-
+  document.getElementById("resultados").innerHTML =  testando100 ;
+  resultados.appendChild(resultados)
+  resultados.appendChild(document.createElement("br"))
+  document.getElementById("resultados").innerHTML =  testando50 ;
+  
+  console.log(testando100,testando50,testando20,testando10,testando5,testando2,testando1)
 }
-// todas as funções pegam o input do usúario, salvam como uma variavel.
-// logo em seguida é criada uma variavel chamada resultados que serve como um header de html
-// nela e mandado para o html o total da operação inputada pelo usuario e o header
+

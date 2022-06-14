139e4f315ca728c32c73a4836aeedaa2b18474e9
edu-porto <eduardo.porto@sou.inteli.edu.br>
2022-06-14 11:52:45 -0300

    falta só o ex5 semana 7

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/ex4/script.js
@@ -0,0 +1,35 @@
+﻿function dias (){
+  var nPessoas = document.getElementById("numero1").value;
+  
+  if (nPessoas <= 50 ){
+
+    var total = parseInt(nPessoas) * parseInt(200)
+    alert ("O total da compra é " + total);
+} 
+  if (nPessoas >50 ){
+    var contas = parseInt(nPessoas) * parseInt(200)
+    var desconto = parseInt (contas) - (parseInt(contas) * 0.4)
+    alert("O total da compra é " + desconto);
+  }
+
+
+  //console.log(total + " desconto" + desconto )
+};
+
+function noites(){
+  var nPessoas = document.getElementById("numero1").value;
+  
+  if (nPessoas <= 50 ){
+
+    var total = parseInt(nPessoas) * parseInt(100)
+    alert ("O total da compra é " + total);
+} 
+  if (nPessoas >50 ){
+    var contas = parseInt(nPessoas) * parseInt(100)
+    var desconto = parseInt (contas) - (parseInt(contas) * 0.2)
+    alert("O total da compra é " + desconto);
+  }
+}
+
+
+//ve como que faz isso dinâmico pra sempre ficar trocando 
---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/ex5/script.js
@@ -0,0 +1,34 @@
+﻿function gravar (){
+  var quantiaAlunos = document.getElementById("numeroAlunos").value;
+  var notasTrabalho = document.getElementById("notaTrabalho").value;
+  var notasProva = document.getElementById("notaProva").value;
+
+
+  var alunosTrabalho = (notasTrabalho).split() ;
+  var alunosProva = (notasProva).split() ;
+
+  // essa função separa os algorismos
+
+  // o javascript não tá entendendo que isso é uma lista e que eu quero separar cada elemento dela 
+  
+
+  console.log(quantiaAlunos + " nota trabalho: " + alunosTrabalho[1] + "notas prova: " + alunosProva)
+
+
+
+
+
+};
+
+
+function fun() {
+  /*Getting the number of text fields*/
+  var no = document.getElementById("idname").value;
+  /*Generating text fields dynamically in the same form itself*/
+  for(var i=0;i<no;i++) {
+      var textfield = document.createElement("input");
+      textfield.type = "text";
+      textfield.value = "";
+      document.getElementById('form').appendChild(textfield);
+  }
+};
